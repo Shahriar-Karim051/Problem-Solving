7a00d73ea889663fb51f5e16246af4a39d1a1fc6
Shahriar-Karim051 <113890642+Shahriar-Karim051@users.noreply.github.com>
2022-09-20 09:35:04 +0600

    Update 1A.py

--- Codeforces/1A.py
+++ Codeforces/1A.py
@@ -1,1 +1,19 @@
 
+n1, m1, a1 = input().split()
+
+n = int(n1)
+m = int(m1)
+a = int(a1)
+
+if n % a == 0:
+    x = n // a
+else:
+    x = (n // a) + 1
+    
+if m % a == 0:
+    y = m // a
+else:
+    y = (m // a) + 1
+
+print(x * y)
+    
